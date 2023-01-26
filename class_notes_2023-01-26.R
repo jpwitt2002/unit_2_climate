52c9456ab9baf6d9a2bcb29c222a5e7cc8a597dc
jpwitt2002 <jpwitt@email.sc.edu>
2023-01-26 11:21:31 -0500

    notes!

---
+++ class_notes_2023-01-26.R
@@ -0,0 +1,16 @@
+# JPW 2023-01-26
+# logical, boolean, if/else
+
+vec = c(1,0,2,1)
+vec[c(T, F, T, F)]
+# = is assignment 
+# == is a test 
+# %in% is a in the group 
+
+# practice logical operators
+
+1>2
+1>c(0, 1, 2)
+c(1, 2, 3)==c(3, 2, 1)
+1 %in% c(1, 2, 3)
+c(1, 3, 5, 7) %in% c(1, 2, 3)
